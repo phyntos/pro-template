2a487ab8747b412e297c1a608b818ae655d7b09f
Eldos Bakytzhan <ebakytzhan@devexpert.kz>
2023-03-27 17:08:12 +0600

    added hooks to index

--- src/pro-template.ts
+++ src/pro-template.ts
@@ -1,9 +1,9 @@
-import ProContainer from './components/ProContainer/ProContainer';
+import ProContainer, { useProContainerTitle, useProFormCardInstance } from './components/ProContainer/ProContainer';
 import ProHeader from './components/ProHeader/ProHeader';
 import ProLogo from './components/ProLogo/ProLogo';
 import ProWorkflow from './components/ProWorkflow/ProWorkflow';
 import ProTabGroup from './components/ProTabGroup/ProTabGroup';
-import ProFormCard from './components/ProFormCard/ProFormCard';
+import ProFormCard, { useProFormCard } from './components/ProFormCard/ProFormCard';
 import ProLogin from './components/ProLogin/ProLogin';
 import ProButton from './components/ProButton/ProButton';
 import ProAntProvider from './components/ProAntProvider';
@@ -20,4 +20,7 @@ export {
     ProFormCard,
     ProButton,
     ProLogin,
+    useProContainerTitle,
+    useProFormCard,
+    useProFormCardInstance,
 };
